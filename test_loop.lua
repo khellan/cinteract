bbec1eb1c5f99287943486a69b33caace0f5f9b2
Knut O. Hellan <kyosha@gmail.com>
2015-05-22 04:40:17 -0400

    Refactor test and train loops
    Move train loop to separate file.
    Move test loop to separate file.

---
+++ test_loop.lua
@@ -0,0 +1,55 @@
+function test()
+   -- local vars
+   local time = sys.clock()
+
+   -- averaged param use?
+   if average then
+      cachedparams = parameters:clone()
+      parameters:copy(average)
+   end
+
+   -- set model to evaluate mode (for modules that differ in training and testing, like Dropout)
+   model:evaluate()
+
+   -- test over test data
+   print('==> testing on test set:')
+   for t = 1,testData:size() do
+      -- disp progress
+      xlua.progress(t, testData:size())
+
+      -- get new sample
+      local input = testData.samples[t]
+      if opt.cuda then input = input:cuda() end
+      local target = testData.labels[t]
+
+      -- test sample
+      local pred = model:forward(input)
+      confusion:add(pred, target)
+   end
+
+   -- timing
+   time = sys.clock() - time
+   time = time / testData:size()
+   print("\n==> time to test 1 sample = " .. (time*1000) .. 'ms')
+
+   -- print confusion matrix
+   print(confusion)
+   totalValid = confusion.totalValid
+
+   -- update log/plot
+   testLogger:add{['% mean class accuracy (test set)'] = confusion.totalValid * 100}
+   if opt.plot then
+      testLogger:style{['% mean class accuracy (test set)'] = '-'}
+      testLogger:plot()
+   end
+
+   -- averaged param use?
+   if average then
+      -- restore parameters
+      parameters:copy(cachedparams)
+   end
+   
+   -- next iteration:
+   confusion:zero()
+   return totalValid
+end
--- trainer.lua
+++ trainer.lua
@@ -45,6 +45,8 @@ dofile('dataset.lua')
 dofile('model.lua')
 dofile('optimizer.lua')
 dofile('loss.lua')
+dofile('train_loop.lua')
+dofile('test_loop.lua')
 
 model = buildModel(opt)
 
@@ -82,168 +84,7 @@ print(model)
 confusion = optim.ConfusionMatrix(classes)
 
 
-----------------------------------------------------------------------
-print '==> defining training procedure'
-
-function train()
-
-   -- epoch tracker
-   epoch = epoch or 1
-
-   -- local vars
-   local time = sys.clock()
-
-   -- set model to training mode (for modules that differ in training and testing, like Dropout)
-   model:training()
-
-   -- shuffle at each epoch
-   shuffle = torch.randperm(trainData:size())
-   -- do one epoch
-   print('==> doing epoch on training data:')
-   print("==> online epoch # " .. epoch .. ' [batchSize = ' .. opt.batchSize .. ']')
-   for t = 1,trainData:size(),opt.batchSize do
-      -- disp progress
-      xlua.progress(t, trainData:size())
-
-      -- create mini batch
-      local inputs = {}
-      local targets = {}
-      for i = t,math.min(t+opt.batchSize-1,trainData:size()) do
-         -- load new sample
-         local input = trainData.samples[shuffle[i]]
-         local target = trainData.labels[shuffle[i]]
-         if opt.type == 'double' then input = input:double()
-         elseif opt.type == 'cuda' then input = input:cuda() end
-         table.insert(inputs, input)
-         table.insert(targets, target)
-      end
-
-      -- create closure to evaluate f(X) and df/dX
-      local feval = function(x)
-                       -- get new parameters
-                       if x ~= parameters then
-                          parameters:copy(x)
-                       end
-
-                       -- reset gradients
-                       gradParameters:zero()
-
-                       -- f is the average of all criterions
-                       local f = 0
-
-                       -- evaluate function for complete mini batch
-                       for i = 1,#inputs do
-                          -- estimate f
-                          local output = model:forward(inputs[i])
-                          local err = criterion:forward(output, targets[i])
-                          f = f + err
-
-                          -- estimate df/dW
-                          local df_do = criterion:backward(output, targets[i])
-                          model:backward(inputs[i], df_do)
-
-                          -- update confusion
-                          confusion:add(output, targets[i])
-                       end
-
-                       -- normalize gradients and f(X)
-                       gradParameters:div(#inputs)
-                       f = f/#inputs
-
-                       -- return f and df/dX
-                       return f,gradParameters
-                    end
-
-      -- optimize on current mini-batch
-      if optimMethod == optim.asgd then
-         _,_,average = optimMethod(feval, parameters, optimState)
-      else
-         optimMethod(feval, parameters, optimState)
-      end
-   end
-
-   -- time taken
-   time = sys.clock() - time
-   time = time / trainData:size()
-   print("\n==> time to learn 1 sample = " .. (time*1000) .. 'ms')
-
-   -- print confusion matrix
-   print(confusion)
-
-   -- update logger/plot
-   trainLogger:add{['% mean class accuracy (train set)'] = confusion.totalValid * 100}
-   if opt.plot then
-      trainLogger:style{['% mean class accuracy (train set)'] = '-'}
-      trainLogger:plot()
-   end
-
-   -- save/log current net
-   local filename = paths.concat(opt.save, 'model.net')
-   os.execute('mkdir -p ' .. sys.dirname(filename))
-   print('==> saving model to '..filename)
-   torch.save(filename, model)
-
-   -- next epoch
-   confusion:zero()
-   epoch = epoch + 1
-   return epoch - 1
-end
-
-function test()
-   -- local vars
-   local time = sys.clock()
-
-   -- averaged param use?
-   if average then
-      cachedparams = parameters:clone()
-      parameters:copy(average)
-   end
-
-   -- set model to evaluate mode (for modules that differ in training and testing, like Dropout)
-   model:evaluate()
-
-   -- test over test data
-   print('==> testing on test set:')
-   for t = 1,testData:size() do
-      -- disp progress
-      xlua.progress(t, testData:size())
-
-      -- get new sample
-      local input = testData.samples[t]
-      if opt.cuda then input = input:cuda() end
-      local target = testData.labels[t]
-
-      -- test sample
-      local pred = model:forward(input)
-      confusion:add(pred, target)
-   end
-
-   -- timing
-   time = sys.clock() - time
-   time = time / testData:size()
-   print("\n==> time to test 1 sample = " .. (time*1000) .. 'ms')
-
-   -- print confusion matrix
-   print(confusion)
-   totalValid = confusion.totalValid
-
-   -- update log/plot
-   testLogger:add{['% mean class accuracy (test set)'] = confusion.totalValid * 100}
-   if opt.plot then
-      testLogger:style{['% mean class accuracy (test set)'] = '-'}
-      testLogger:plot()
-   end
-
-   -- averaged param use?
-   if average then
-      -- restore parameters
-      parameters:copy(cachedparams)
-   end
-   
-   -- next iteration:
-   confusion:zero()
-   return totalValid
-end
+print '==> training'
 
 currentEpoch = 0
 bestEpoch = 0
@@ -257,7 +98,7 @@ while currentEpoch < opt.epochs do
    end
 end
 
-print('--- --- --- --- ---')
+print('Summary:')
 print('Best result: ' .. (bestTotalValid * 100) .. '%')
 print('Best epoch : ' .. bestEpoch)
-print('--- --- --- --- ---')
+
